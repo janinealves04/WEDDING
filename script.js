cb2465accc79266e587b1ccb830a9da6b784fa6c
Janinealves04 <Janinealves.sil@gmail.com>
2025-07-10 10:56:05 -0300

    Criando transição para exibição do conteúdo, adicionando negrito as seções no navbar e criando o footer.

---
+++ script.js
@@ -0,0 +1,31 @@
+function mostrarSecao(id){
+  // esconder as seções
+  const secoes = document.querySelectorAll('.secao');
+  secoes.forEach(secao => {
+    secao.style.display = 'none';
+   // secao.classList.remove('mostrar-transicao'); // Remove transição anterior
+  });
+
+  // mostrar seção desejada e aplicar transição
+  const secaoSelecionada = document.getElementById(id);
+  if (secaoSelecionada) {
+    secaoSelecionada.style.display = 'block';
+    setTimeout(() => {
+      secaoSelecionada.classList.add('mostrarTransicao');
+    }, 10);
+  }
+
+  // atualizar o menu
+  const links = document.querySelectorAll('nav a');
+  links.forEach(link => {
+    link.classList.remove('active');
+    if (link.getAttribute('onclick')?.includes(id)){
+      link.classList.add('active');
+    }
+  });
+}
+
+// mostrar home ao carregar
+window.onload = () => {
+  mostrarSecao('home');
+};
